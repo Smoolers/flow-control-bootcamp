f5667771ec663d400461fa6aef53794fad904e9d
Martin Hagegård <martin.hagegard@gmail.com>
2025-05-17 15:17:35 +0200

    Added index.html and index.js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 20;
+
+if (a < b) {
+  console.log("a är lägre än b");
+}
+
+a = "apple";
+b = "pear";
+
+if (a === b) {
+  console.log("a är samma som b");
+} else {
+  console.log("a är inte samma som b");
+}
+
+if (a != b) {
+  console.log("a är inte samma som b");
+}
+
+const number = prompt("Skriv in din ålder");
+//Det användaren skriver in sparas i variabeln number
